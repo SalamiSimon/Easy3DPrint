40ddc48719ca94e0c63293a9983067195da4b6ec
Simon Agerhjelm <simonagerhjelm@outlook.com>
2024-04-15 12:59:44 +0200

    Initial commit

---
+++ ApplicationSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DPrint_SW
+{
+    internal class ApplicationSettings
+    {
+        public class CuraSettings
+        {
+            //private CuraSettings curaSettings = new CuraSettings();
+
+            public string Path { get; set; } = "";
+            public string FileType { get; set; } = "";
+
+            public CuraSettings() { }
+
+            public CuraSettings(string path, string fileType)
+            {
+                Path = path;
+                FileType = fileType;
+            }
+        }
+    }
+}
